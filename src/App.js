dfd8ba0b5296e992011e5ca470f9599108ba8659
Vũ Tuấn Anh <vtanh1905@gmail.com>
2020-03-14 16:23:39 +0700

    test alias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import backgroundImage from '@assets/backgroud.jpg'
 
 function App() {
   return (
     <div>
-      Hello world
-      {process.env.PORT}
+      <img src={backgroundImage} />
+      By Vu Tuan Anh
     </div>
   )
 }
